253ed5e9b499b488c1d987e65f32804b0857ae2f
amineacilan <amineacila@gmail.com>
2022-12-01 15:36:25 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
-
+// test
 const DUMMY_EXPENSES = [
   {
     id: "e1",
